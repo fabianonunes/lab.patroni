8401716f78baeb171f85afc8f2da6074acc1f0cc
Fabiano Parente <fabiano@nunes.me>
2025-07-28 16:44:51 -0300

    não passa senhas via env processo a baixo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ gomplate --file patroni.yaml.tpl \
   --datasource postgresql.parameters=postgresql.parameters.yaml \
   --out patroni.yaml
 
+unset PATRONI_SUPERUSER_PASSWORD PATRONI_REPLICATION_PASSWORD
+
 chown postgres /var/lib/postgresql/data
 chmod 700 /var/lib/postgresql/data
 
